d0caa269a4884cbdba1d62757ef4922a8117fc61
michael-2509 <61965542+michael-2509@users.noreply.github.com>
2020-03-27 09:37:59 +0100

    Add files via upload

---
+++ task 3(js).js
@@ -0,0 +1,14 @@
+let name = "michael"
+var films =["js", "css", "html"]; 
+console.log("my name is:" +name);
+console.log(films[0]);
+console.log(films[1]);
+console.log(films[2]);
+
+
+for (let i=0; i<=200;i++){
+    if (i%2!==0){
+        console.log(i);
+    }}
+
+    y
